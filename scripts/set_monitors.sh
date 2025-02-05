014591725bc01b04caa446c2770ce6085f50d796
zyon <32070378+zyoNoob@users.noreply.github.com>
2025-02-05 19:09:24 +0530

    fixed monitor bullshit

--- scripts/set_monitors.sh
+++ scripts/set_monitors.sh
@@ -14,10 +14,12 @@ if [ "$DISPLAY_COUNT" -gt 1 ]; then
     # DUAL MONITOR SETUP
     PRIMARY_MONITOR=${PRIMARY_MONITOR:-"DP-4"}
     SECONDARY_MONITOR=${SECONDARY_MONITOR:-"DP-2"}
+    TERTIARY_MONITOR=${TERTIARY_MONITOR:-"HDMI-0"}
     
     xrandr --fb 6720x2160 \
         --output $SECONDARY_MONITOR --mode 1920x1080 --pos 0x270 --rate 144 --scale 1.5x1.5 \
-        --output $PRIMARY_MONITOR --mode 3840x2160 --pos 2880x0 --primary --rate 144
+        --output $PRIMARY_MONITOR --mode 3840x2160 --pos 2880x0 --primary --rate 144 \
+        --output $TERTIARY_MONITOR --off
 else
     echo "Detected single display, configuring single monitor setup..."
     # SINGLE MONITOR SETUP
