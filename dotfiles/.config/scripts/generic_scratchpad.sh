f824bad0e37cb4f3d09ab0fcab4c30481ffcafaa
zyon <32070378+zyoNoob@users.noreply.github.com>
2025-11-25 06:32:51 +0530

    Refactor generic scratchpad script to use window ID

--- dotfiles/.config/scripts/generic_scratchpad.sh
+++ dotfiles/.config/scripts/generic_scratchpad.sh
@@ -7,23 +7,19 @@
 NAMED_INSTANCES="scratchpad-terminal|scratchpad-music|scratchpad-monitor|scratchpad-files"
 NAMED_CLASSES="gnome-calculator|discord|obsidian"
 
-# Get all scratchpad windows
-SCRATCHPAD_WINDOWS=$(i3-msg -t get_tree | jq -r '
-  .. | 
-  select(.type? == "con" and .name? != null) | 
-  select(.floating? == "user_on" or .floating? == "auto_on") |
-  select(any(.nodes[]?; .scratchpad_state? == "fresh" or .scratchpad_state? == "changed") or .scratchpad_state? == "fresh" or .scratchpad_state? == "changed") |
-  select(
-    (.window_properties.instance? | test("^('"$NAMED_INSTANCES"')$") | not) and
-    (.window_properties.class? | test("^('"$NAMED_CLASSES"')$"; "i") | not)
-  ) |
-  .id
-' | head -1)
+# Get the first generic scratchpad window (not in our named list)
+WINDOW_ID=$(i3-msg -t get_tree | jq -r '
+  [.. | select(.scratchpad_state? == "fresh" or .scratchpad_state? == "changed") | 
+   .. | 
+   select(.window?) | 
+   select(
+     (.window_properties.instance? | test("^('"$NAMED_INSTANCES"')$") | not) and
+     (.window_properties.class? | test("^('"$NAMED_CLASSES"')$"; "i") | not)
+   ) | 
+   .window
+  ] | first // empty
+')
 
-if [ -n "$SCRATCHPAD_WINDOWS" ]; then
-    i3-msg "[con_id=$SCRATCHPAD_WINDOWS] scratchpad show, move position center"
-else
-    # If no generic scratchpad found, just call the normal scratchpad show
-    # This will work if windows are in scratchpad but jq query didn't catch them
-    i3-msg "scratchpad show"
+if [ -n "$WINDOW_ID" ]; then
+    i3-msg "[id=$WINDOW_ID] scratchpad show, move position center"
 fi
